f9734e95f2dfcbc83cf715fbc539d465ba3d9d42
Grigori Fursin <grigori.fursin@cTuning.org>
2015-03-10 00:01:10 +0100

    updating LLVM env description

--- soft/compiler.llvm.win/customize.py
+++ soft/compiler.llvm.win/customize.py
@@ -16,14 +16,13 @@ def setup(i):
               cfg          - dict of the soft entry
               tags         - list of tags
               env          - environment
+              deps         - resolved deps
 
               interactive  - if 'yes', ask questions
 
               (customize)  - external params for possible customization:
 
-                             tool_prefix             - prefixing all tool names (XYZ-gcc)
-                             linking_for_retargeting - if !='', add to env[CK_LD_FLAGS_EXTRA]
-                             add_m32                 - if 'yes' and target OS is 32 bit, add -m32
+                             target_arm - if 'yes', target ARM
             }
 
     Output: {
@@ -32,77 +31,104 @@ def setup(i):
               (error)      - error text if return > 0
 
               bat        - prepared string for bat file
-              env          - updated environment
-              tags         - updated tags
             }
 
     """
 
     import os
 
+    # Get variables
     s=''
 
     iv=i.get('interactive','')
-    if iv=='yes': print ('')
 
-    deps=i.get('deps',{})
     env=i.get('env',{})
     cfg=i.get('cfg',{})
-    cus=i.get('customize',{})
+    deps=i.get('deps',{})
     tags=i.get('tags',[])
+    cus=i.get('customize',{})
 
-    tbits=i.get('target_os_bits','')
-
+    host_d=i.get('host_os_dict',{})
     target_d=i.get('target_os_dict',{})
-    wb=target_d.get('windows_base','')
+    winh=host_d.get('windows_base','')
+    win=target_d.get('windows_base','')
+    remote=target_d.get('remote','')
+    mingw=target_d.get('mingw','')
+    tbits=target_d.get('bits','')
 
-    envp=cfg.get('env_prefix','')
+    envp=cus.get('env_prefix','')
+    pi=cus.get('path_install','')
 
-    # Ask a few more questions
-    prefix=cus.get('tool_prefix','')
-    if prefix=='' and iv=='yes':
-       prefix=raw_input('Compiler name prefix, if needed (such as "arm-none-linux-gnueabi-"): ')
+    ############################################################
+    if winh=='yes':
+       # Ask a few more questions
+       prefix_configured=cus.get('tool_prefix_configured','')
+       prefix=cus.get('tool_prefix','')
+       if prefix_configured!='yes' and iv=='yes':
+          if prefix!='':
+             ck.out('Current compiler name prefix: '+prefix)
+          else:
+             prefix=raw_input('Compiler name prefix, if needed (such as "arm-none-linux-gnueabi-"): ')
+             cus['tool_prefix_configured']='yes'
 
-    retarget=cus.get('linking_for_retargeting','')
-    if retarget=='' and iv=='yes':
-       x=raw_input('Using retargeting (for example, for ARM) (y/N)? ')
-       x=x.lower()
-       if x!='' and x=='y' or x=='yes':
-          y='-Wl,-dynamic-linker,/data/local/tmp/ld-linux.so.3 -Wl,--rpath -Wl,/data/local/tmp -lm -ldl'
-          retarget=raw_input('LD extra flags for retargeting (or Enter for "'+y+'"): ')
-          if retarget=='': retarget=y
+       if prefix!='':
+          env['CK_COMPILER_PREFIX']=prefix
+          cus['tool_prefix']=prefix
+          cus['tool_prefix_configured']='yes'
 
-    if retarget!='':
-       env['CK_LD_FLAGS_EXTRA']=retarget
-       if 'retargeted' not in tags: tags.append('retargeted')
+       for k in env:
+           v=env[k]
+           v=v.replace('$#tool_prefix#$',prefix)
+           env[k]=v
 
-    add_m32=cus.get('add_m32','')
-    if add_m32=='' and iv=='yes':
-       x=raw_input('Target OS is 32 bit. Add -m32 to compilation flags (y/N)? ')
-       x=x.lower()
-       if x=='y' or x=='yes': add_m32='yes'
+       retarget=cus.get('retarget','')
+       lfr=cus.get('linking_for_retargeting','')
+       if retarget=='' and iv=='yes':
+          x=raw_input('Using retargeting (for example, for ARM) (y/N)? ')
+          x=x.lower()
+          if x!='' and x=='y' or x=='yes':
+             retarget='yes'
+             cus['retarget']='yes'
+             if 'retargeted' not in tags: tags.append('retargeted')
 
-    if prefix!='':
-       env['CK_COMPILER_PREFIX']=prefix
+             if lfr=='' and iv=='yes':
+                y='-Wl,-dynamic-linker,/data/local/tmp/ld-linux.so.3 -Wl,--rpath -Wl,/data/local/tmp -lm -ldl'
+                lfr=raw_input('LD extra flags for retargeting (or Enter for "'+y+'"): ')
+                if lfr=='': lfr=y
 
-    for k in env:
-        v=env[k]
+          else:
+             cus['retarget']='no'
 
-        v=v.replace('$#tool_prefix#$',prefix)
+       if retarget=='yes' and lfr!='':
+          cus['linking_for_retargeting']=lfr
+          env['CK_LD_FLAGS_EXTRA']=lfr
 
-        if wb=='yes':
-           if k=='CK_COMPILER_FLAGS_OBLIGATORY': 
-              v=v+' -DWINDOWS'
-              if tbits=='32': 
-                 if add_m32=='yes': v+=' -m32'
-              else:
-                 v+=' -m64'
 
-              z1=cus.get('add_to_ck_compiler_flags_obligatory','')
-              if z1!='': v+=' '+z1
+       add_m32=cus.get('add_m32','')
+       if add_m32=='' and iv=='yes' and tbits=='32':
+          x=raw_input('Target OS is 32 bit. Add -m32 to compilation flags (y/N)? ')
+          x=x.lower()
+          if x=='y' or x=='yes': 
+             add_m32='yes'
+             cus['add_m32']='yes'
 
-           if k=='CK_CXX': v=v+' -fpermissive'   
+       x=env.get('CK_COMPILER_FLAGS_OBLIGATORY','')
+       if remote!='yes':
+          if x.find('-DWINDOWS')<0: 
+             x+=' -DWINDOWS' 
+       if tbits=='32' and add_m32=='yes' and x.find('-m32')<0: 
+          x+=' -m32' 
+       env['CK_COMPILER_FLAGS_OBLIGATORY']=x
 
-        env[k]=v
+       if mingw=='yes': env['CK_MAKE']='mingw32-make'
 
-    return {'return':0, 'bat':s, 'env':env, 'tags':tags, 'deps':deps}
+       x=env.get('CK_CXX','')
+       if x!='' and x.find('-fpermissive')<0:
+          x+=' -fpermissive'
+       env['CK_CXX']=x
+
+       x=cus.get('add_extra_path','')
+       if x!='':
+          s+='\nset PATH='+pi+x+';%PATH%\n\n'
+
+    return {'return':0, 'bat':s, 'env':env, 'tags':tags}
