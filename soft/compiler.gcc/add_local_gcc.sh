0b84739ca0a4c4e0648900382b8c3fb41fe19ba6
Grigori Fursin <Grigori.Fursin@cTuning.org>
2015-03-25 15:25:43 +0100

    adding demo scripts to configure local GCC and LLVM for further auto-tuning

---
+++ soft/compiler.gcc/add_local_gcc.sh
@@ -0,0 +1,1 @@
+ck setup soft:compiler.gcc --skip_path target_os=$1 env_data_uoa=compiler-gcc-local-$1 env_repo_uoa=ck-auto-tuning --env-new
---
+++ soft/compiler.llvm/add_local_llvm.sh
@@ -0,0 +1,1 @@
+ck setup soft:compiler.llvm --skip_path target_os=$1 env_data_uoa=compiler-linux-local-$1 env_repo_uoa=ck-auto-tuning --env-new
