d7d9115aab6d9de8f690bb47def7717c6545cab5
Grigori Fursin <Grigori.Fursin@cTuning.org>
2015-03-26 18:27:42 +0100

    Adding CUDA OpenCL Lib description for Windows

---
+++ soft/lib.opencl.cuda/customize.py
@@ -0,0 +1,93 @@
+#
+# Collective Knowledge (individual environment - setup)
+#
+# See CK LICENSE.txt for licensing details
+# See CK Copyright.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://cTuning.org/lab/people/gfursin
+#
+
+##############################################################################
+# setup environment setup
+
+def setup(i):
+    """
+    Input:  {
+              cfg          - dict of the soft entry
+              tags         - list of tags
+              env          - environment
+              deps         - dependencies
+
+              interactive  - if 'yes', ask questions
+
+              (customize)  - external params for possible customization:
+
+                             target_arm - if 'yes', target ARM
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+
+              bat          - prepared string for bat file
+              env          - updated environment
+              deps         - updated dependencies
+              tags         - updated tags
+
+              path         - install path
+            }
+
+    """
+
+    import os
+
+    # Get variables
+    s=''
+
+    iv=i.get('interactive','')
+
+    env=i.get('env',{})
+    cfg=i.get('cfg',{})
+    deps=i.get('deps',{})
+    tags=i.get('tags',[])
+    cus=i.get('customize',{})
+
+    target_d=i.get('target_os_dict',{})
+    win=target_d.get('windows_base','')
+    remote=target_d.get('remote','')
+    mingw=target_d.get('mingw','')
+    tbits=target_d.get('bits','')
+
+    envp=cus.get('env_prefix','')
+    pi=cus.get('path_install','')
+
+    ################################################################
+    if win=='yes':
+       ext='Win64'
+       if tbits=='32': ext='Win32'
+
+       cus['path_bin']=pi+'\\bin\\'
+       cus['path_lib']=pi+'\\lib\\'+ext
+
+       cus['static_lib']='OpenCL.lib'
+
+       cus['include_name']='CL\\opencl.h'
+
+    else:
+       x=''
+       if tbits=='64': x='64'
+
+       cus['path_bin']=pi+'/bin'
+       cus['path_lib']=pi+'/lib'+x
+
+       cus['include_name']='CL/opencl.h'
+
+       cus['static_lib']='libOpenCL.so'
+       cus['dynamic_lib']='libOpenCL.so'
+
+    env['CK_ENV_LIB_OPENCL_INCLUDE_NAME']=cus.get('include_name','')
+    env['CK_ENV_LIB_OPENCL_STATIC_NAME']=cus.get('static_lib','')
+    env['CK_ENV_LIB_OPENCL_DYNAMIC_NAME']=cus.get('dynamic_lib','')
+
+    return {'return':0, 'bat':s}
