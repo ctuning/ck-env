ce7409cd367aa1a9663a5f910237604a3357b3ca
Grigori Fursin <grigori.fursin@cTuning.org>
2015-03-27 19:39:17 +0100

    Adding OpenME plugin framework for JAVA for plugin-based auto-tuning

---
+++ soft/lib.openme.java/customize.py
@@ -0,0 +1,86 @@
+#
+# Collective Knowledge (individual environment - setup)
+#
+# See CK LICENSE.txt for licensing details
+# See CK Copyright.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://cTuning.org/lab/people/gfursin
+#
+
+##############################################################################
+# setup environment setup
+
+def setup(i):
+    """
+    Input:  {
+              cfg          - dict of the soft entry
+              tags         - list of tags
+              env          - environment
+              deps         - dependencies
+
+              interactive  - if 'yes', ask questions
+
+              (customize)  - external params for possible customization:
+
+                             target_arm - if 'yes', target ARM
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+
+              bat          - prepared string for bat file
+              env          - updated environment
+              deps         - updated dependencies
+              tags         - updated tags
+
+              path         - install path
+            }
+
+    """
+
+    import os
+
+    # Get variables
+    s=''
+
+    iv=i.get('interactive','')
+
+    env=i.get('env',{})
+    cfg=i.get('cfg',{})
+    deps=i.get('deps',{})
+    tags=i.get('tags',[])
+    cus=i.get('customize',{})
+
+    target_d=i.get('target_os_dict',{})
+    win=target_d.get('windows_base','')
+    remote=target_d.get('remote','')
+    mingw=target_d.get('mingw','')
+    tbits=target_d.get('bits','')
+
+    envp=cus.get('env_prefix','')
+    pi=cus.get('path_install','')
+
+    ################################################################
+    #TBD
+#    if win=='yes':
+#       if remote=='yes': 
+#          sext='.a'
+#          dext='.so'
+#       else:
+#          sext='.lib'
+#          dext='.dll'
+#    else:
+#       sext='.a'
+#       dext='.so'
+#
+#    cus['include_name']='openme.h'
+#    cus['static_lib']='libopenme'+sext
+#    cus['dynamic_lib']='libopenme'+dext
+#
+#    env['CK_ENV_LIB_OPENME_INCLUDE_NAME']=cus.get('include_name','')
+#    env['CK_ENV_LIB_OPENME_STATIC_NAME']=cus.get('static_lib','')
+#    env['CK_ENV_LIB_OPENME_DYNAMIC_NAME']=cus.get('dynamic_lib','')
+
+    return {'return':0, 'bat':s}
