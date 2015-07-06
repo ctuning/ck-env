57f6daec236fe7ab6b5e3fd00158f1bc14ca702f
Grigori Fursin <grigori.fursin@cTuning.org>
2015-07-06 12:13:52 -0400

    adding vtune description to reproduce past R&D on machine learning for autotuning using hardware counters

---
+++ soft/perf.vtune/customize.py
@@ -0,0 +1,75 @@
+#
+# Collective Knowledge (individual environment - setup)
+#
+# See CK LICENSE.txt for licensing details
+# See CK Copyright.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://cTuning.org/lab/people/gfursin
+#
+
+##############################################################################
+# setup environment setup
+
+def setup(i):
+    """
+    Input:  {
+              cfg          - dict of the soft entry
+              tags         - list of tags
+              env          - environment
+              deps         - dependencies
+
+              interactive  - if 'yes', ask questions
+
+              (customize)  - external params for possible customization:
+
+                             target_arm - if 'yes', target ARM
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+
+              bat          - prepared string for bat file
+              env          - updated environment
+              deps         - updated dependencies
+              tags         - updated tags
+
+              path         - install path
+            }
+
+    """
+
+    import os
+
+    # Get variables
+    s=''
+
+    iv=i.get('interactive','')
+
+    env=i.get('env',{})
+    cfg=i.get('cfg',{})
+    deps=i.get('deps',{})
+    tags=i.get('tags',[])
+    cus=i.get('customize',{})
+
+    host_d=i.get('host_os_dict',{})
+    target_d=i.get('target_os_dict',{})
+    winh=host_d.get('windows_base','')
+    win=target_d.get('windows_base','')
+    mingw=target_d.get('mingw','')
+    tbits=target_d.get('bits','')
+
+    envp=cus.get('env_prefix','')
+    pi=cus.get('path_install','')
+
+    ################################################################
+    s+='\n'
+    s+=host_d.get('rem','')+' Setting Intel compiler environment\n'
+
+    x='bat'
+    if winh!='yes': x='sh'
+
+    s+=host_d.get('env_call','')+' "'+pi+host_d.get('dir_sep','')+'amplxe-vars.'+x+'" '+'\n\n'
+
+    return {'return':0, 'bat':s}
