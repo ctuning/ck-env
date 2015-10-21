162ce7758917104256afb6983f2f9d91d139a659
Grigori Fursin <Grigori.Fursin@cTuning.org>
2015-10-21 17:33:34 +0200

    fixing pencil compiler descriptions

---
+++ soft/bench.pencil/customize.py
@@ -0,0 +1,73 @@
+#
+# Collective Knowledge (individual environment - setup)
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://fursin.net
+#
+
+##############################################################################
+# setup environment setup
+
+import sys
+if sys.version_info[0]>2:
+   def raw_input(i):
+       return input(i)
+
+def setup(i):
+    """
+    Input:  {
+              cfg          - dict of the soft entry
+              tags         - list of tags
+              env          - environment
+              deps         - resolved deps
+
+              interactive  - if 'yes', ask questions
+
+              (customize)  - external params for possible customization:
+
+                             target_arm - if 'yes', target ARM
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+
+              bat        - prepared string for bat file
+            }
+
+    """
+
+    import os
+
+    # Get variables
+    s=''
+
+    iv=i.get('interactive','')
+
+    env=i.get('env',{})
+    cfg=i.get('cfg',{})
+    deps=i.get('deps',{})
+    tags=i.get('tags',[])
+    cus=i.get('customize',{})
+
+    target_d=i.get('target_os_dict',{})
+    win=target_d.get('windows_base','')
+    mingw=target_d.get('mingw','')
+    tbits=target_d.get('bits','')
+
+    envp=cus.get('env_prefix','')
+    pi=cus.get('path_install','')
+
+    ############################################################
+    if win=='yes':
+       return {'return':1, 'error':'Pencil is not supporting Windows!'}
+    else:
+       s+='\n'
+       s+='# Setting environment\n'
+
+       s+='export PATH='+pi+'/scripts:$PATH\n'
+
+    return {'return':0, 'bat':s}
