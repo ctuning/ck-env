37d35929091bc15b102a0be30ce7e342dbd5ff70
Grigori Fursin <grigori.fursin@cTuning.org>
2016-05-06 16:59:22 -0400

    adding Julia compiler (after MIT visit)

---
+++ soft/compiler.julia/customize.py
@@ -0,0 +1,55 @@
+#
+# Collective Knowledge (individual environment - setup)
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://fursin.net
+#
+
+import os
+
+##############################################################################
+# customize directories to automatically find and register software
+
+def dirs(i):
+    return {'return':0}
+
+##############################################################################
+# parse software version
+
+def parse_version(i):
+
+    lst=i['output']
+
+    ver=''
+
+    for q in lst:
+        q=q.strip()
+        if q!='' and q.startswith('julia version '):
+           ver=q[14:]
+           break
+
+    return {'return':0, 'version':ver}
+
+##############################################################################
+# setup environment
+
+def setup(i):
+
+    s=''
+
+    cus=i['customize']
+    env=i['env']
+
+    fp=cus.get('full_path','')
+
+    ep=cus.get('env_prefix','')
+    if ep!='' and fp!='':
+       p1=os.path.dirname(fp)
+       p2=os.path.dirname(p1)
+
+       env[ep]=p2
+       env[ep+'_BIN']=p1
+
+    return {'return':0, 'bat':s}
