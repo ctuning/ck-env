4aad8affdad05cd94e9fbe2b1f41c51623a7bc30
Grigori Fursin <Grigori.Fursin@cTuning.org>
2016-07-25 11:40:54 +0100

    fixing flags for CUDA (7.5 and 8.0)

--- soft/compiler.cuda/customize.py
+++ soft/compiler.cuda/customize.py
@@ -134,7 +134,7 @@ def setup(i):
     env['CUDA_INSTALL_DIR']=p2
 
     opt1='--relaxed-constexpr'
-    if len(sver)>1 and sver[0]>6 and sver[1]>0:
+    if len(sver)>1 and ((sver[0]==7 and sver[1]>=5) or sver[0]>7):
        opt1='--expt-relaxed-constexpr'
     env['CK_OPT_RELAXED_CONSTEXPR']=opt1
 
