6855c1f6825c5e8e9292919c2ee0d0c76103fe94
Grigori Fursin <Grigori.Fursin@cTuning.org>
2016-09-20 17:36:19 +0200

    added soft:tool.adb (for ARM workload automation)

---
+++ soft/tool.adb/customize.py
@@ -0,0 +1,72 @@
+#
+# Collective Knowledge (individual environment - setup)
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://fursin.net
+#
+
+import os
+
+##############################################################################
+# customize directories to automatically find and register software
+
+def dirs(i):
+    return {'return':0}
+
+##############################################################################
+# limit directories 
+
+def limit(i):
+
+    dr=i.get('list',[])
+    drx=[]
+
+    for q in dr:
+        if q.find('X11')<0:
+           drx.append(q)
+
+    return {'return':0, 'list':drx}
+
+
+##############################################################################
+# parse software version
+
+def parse_version(i):
+
+    lst=i['output']
+
+    ver=''
+
+    for q in lst:
+        q=q.strip()
+        if q!='':
+           j=q.lower().find('version ')
+           if j>0:
+              ver=q[j+8:]
+              break
+
+    return {'return':0, 'version':ver}
+
+##############################################################################
+# setup environment
+
+def setup(i):
+
+    s=''
+
+    cus=i['customize']
+    env=i['env']
+
+    fp=cus.get('full_path','')
+
+    ep=cus.get('env_prefix','')
+    if ep!='' and fp!='':
+       p1=os.path.dirname(fp)
+       p2=os.path.dirname(p1)
+
+       env[ep]=p2
+       env[ep+'_BIN']=p1
+
+    return {'return':0, 'bat':s}
