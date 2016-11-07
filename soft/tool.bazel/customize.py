651c01dabb2910fb929cc683f8e1ba0363354ab1
Grigori Fursin <Grigori.Fursin@cTuning.org>
2016-11-07 21:08:45 +0000

    added soft description 'tool.bazel' (useful to recompile Google's TensorFlow)

---
+++ soft/tool.bazel/customize.py
@@ -0,0 +1,69 @@
+#
+# Collective Knowledge (individual environment - setup)
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://fursin.net
+#
+
+import os
+
+##############################################################################
+# customize directories to automatically find and register software
+
+def dirs(i):
+    return {'return':0}
+
+##############################################################################
+# parse software version
+
+def parse_version(i):
+
+    lst=i['output']
+
+    ver=''
+    if len(lst)>0:
+        x=lst[0]
+
+        if x.startswith('Build label:'):
+            ver=x[11:].strip()
+
+    return {'return':0, 'version':ver}
+
+##############################################################################
+# setup environment
+
+def setup(i):
+
+    s=''
+
+    cus=i['customize']
+    env=i['env']
+
+    host_d=i.get('host_os_dict',{})
+    target_d=i.get('host_os_dict',{})
+
+    tbits=target_d.get('bits','')
+
+    winh=host_d.get('windows_base','')
+
+    # Get paths
+    fp=cus['full_path']
+    ep=cus['env_prefix']
+
+    pb=os.path.dirname(fp)
+    p2=os.path.dirname(pb)
+
+    px=os.path.join(p2,'bazel-bin','scripts','bazel-complete.bash')
+    if not os.path.isfile(px):
+        return {'return':1, 'error':'can\'t find bazel-complete.bash ('+px+')'}
+
+    if winh!='yes':
+        s+='\nsource '+px+'\n\n'
+
+    env[ep]=p2
+    env[ep+'_BIN']=pb
+    env[ep+'_INIT']=px
+
+    return {'return':0, 'bat':s}
