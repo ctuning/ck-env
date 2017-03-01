1f15f15a3b5df4767f7e2d2e4eed269e2d08716e
Grigori Fursin <grigori.fursin@cTuning.org>
2017-03-01 12:01:24 +0100

    added extra flags to GCC with -std=xyz

--- soft/compiler.gcc/customize.py
+++ soft/compiler.gcc/customize.py
@@ -185,6 +185,8 @@ def setup(i):
       "CK_COMPILER_FLAGS_OBLIGATORY": "", 
       "CK_COMPILER_FLAG_CPP11": "-std=c++11", 
       "CK_COMPILER_FLAG_CPP0X": "-std=c++0x", 
+      "CK_COMPILER_FLAG_STD90": "-std=c90", 
+      "CK_COMPILER_FLAG_STD99": "-std=c99", 
       "CK_COMPILER_FLAG_GPROF": "-pg", 
       "CK_COMPILER_FLAG_OPENMP": "-fopenmp", 
       "CK_COMPILER_FLAG_PLUGIN": "-fplugin=", 
