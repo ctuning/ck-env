9d2f9ca7a4239b09b1f6a92429d04042c0e190a8
Grigori Fursin <grigori.fursin@cTuning.org>
2017-03-16 22:17:17 +0100

    - added soft:utils.unix.win (set of Unix Utils on Windows useful to compile OpenBLAS on Windows)

---
+++ soft/utils.unix.win/customize.py
@@ -0,0 +1,45 @@
+#
+# Collective Knowledge (individual environment - setup)
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://fursin.net
+#
+
+import os
+
+##############################################################################
+# customize directories to automatically find and register software
+
+def dirs(i):
+    return {'return':0}
+
+##############################################################################
+# parse software version
+
+def parse_version(i):
+    ver=''
+    return {'return':0, 'version':ver}
+
+##############################################################################
+# setup environment
+
+def setup(i):
+
+    s=''
+
+    cus=i['customize']
+    env=i['env']
+
+    fp=cus.get('full_path','')
+
+    ep=cus.get('env_prefix','')
+    if ep!='' and fp!='':
+       p1=os.path.dirname(fp)
+       p2=os.path.dirname(p1)
+
+       env[ep]=p2
+       env[ep+'_BIN']=p1
+
+    return {'return':0, 'bat':s}
