f75433e69d027cd8b0982db76c024db9cbd3816b
Grigori Fursin <grigori.fursin@cTuning.org>
2017-03-27 14:58:09 +0200

    fixing libjpeg soft description

--- soft/lib.libjpeg/customize.py
+++ soft/lib.libjpeg/customize.py
@@ -71,14 +71,55 @@ def setup(i):
     hproc=hosd.get('processor','')
     tproc=tosd.get('processor','')
 
+    win=tosd.get('windows_base','')
+    mingw=tosd.get('mingw','')
     remote=tosd.get('remote','')
     tbits=tosd.get('bits','')
 
     env=i['env']
 
-    pi=os.path.dirname(fp)
-
     ep=cus['env_prefix']
+
+    plib=os.path.dirname(fp)
+    pi=os.path.dirname(plib)
+
     env[ep]=pi
+
+    pname=os.path.basename(fp)
+    j=pname.rfind('.')
+    if j>0:
+       pname=pname[:j]
+
+    cus['path_lib']=plib
+    cus['path_include']=pi+sdirs+'include'
+
+    ################################################################
+    if win=='yes':
+       if remote=='yes' or mingw=='yes': 
+          sext='.a'
+          dext='.so'
+       else:
+          sext='.lib'
+          dext='.dll'
+    else:
+       sext='.a'
+       dext='.so'
+
+    r = ck.access({'action': 'lib_path_export_script',
+                   'module_uoa': 'os', 
+                   'host_os_dict': hosd, 
+                   'lib_path': cus.get('path_lib','')})
+    if r['return']>0: return r
+    s+=r['script']
+
+    x=os.path.join(plib, pname+sext)
+    if os.path.isfile(x):
+       cus['static_lib']=pname+sext
+       env[ep+'_STATIC_NAME']=pname+sext
+
+    x=os.path.join(plib, pname+dext)
+    if os.path.isfile(x):
+       cus['dynamic_lib']=pname+dext
+       env[ep+'_DYNAMIC_NAME']=pname+dext
 
     return {'return':0, 'bat':s}
