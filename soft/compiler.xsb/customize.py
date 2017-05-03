77ed099216e3085684096709bf763fa93c892824
Grigori Fursin <grigori.fursin@cTuning.org>
2017-05-03 09:11:11 +0200

    fixing typo in xsb

--- soft/compiler.xsb/customize.py
+++ soft/compiler.xsb/customize.py
@@ -107,23 +107,24 @@ def setup(i):
 
     # check target
     p=os.path.join(pi,'config')
-    ld=os.listdir(p)
+    if os.path.isdir(p):
+       ld=os.listdir(p)
 
-    if len(ld)>0:
-       if winh=='yes':
-          r=ck.access({'action':'convert_to_cygwin_paths',
-                       'module_uoa':'os',
-                       'paths':{'pi':pi, 'bin':p1, 'ld0':ld[0]}})
-          if r['return']>0: return r
-          pp=r['paths']
+       if len(ld)>0:
+          if winh=='yes':
+             r=ck.access({'action':'convert_to_cygwin_paths',
+                          'module_uoa':'os',
+                          'paths':{'pi':pi, 'bin':p1, 'ld0':ld[0]}})
+             if r['return']>0: return r
+             pp=r['paths']
 
-          env[ep]=pp['pi']
-          env[ep+'_BIN']=pp['bin']
+             env[ep]=pp['pi']
+             env[ep+'_BIN']=pp['bin']
 
-          s+='set XSB_DIR='+pp['pi']+'\n\n'
-          s+='set XSB_DIR_ADD='+pp['ld0']+'\n\n'
-       else:
-          s+='export XSB_DIR='+pi+'\n\n'
-          s+='export XSB_DIR_ADD='+ld[0]+'\n\n'
+             s+='set XSB_DIR='+pp['pi']+'\n\n'
+             s+='set XSB_DIR_ADD='+pp['ld0']+'\n\n'
+          else:
+             s+='export XSB_DIR='+pi+'\n\n'
+             s+='export XSB_DIR_ADD='+ld[0]+'\n\n'
 
     return {'return':0, 'bat':s}
