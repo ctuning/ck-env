b002074ab4d23f494913bfbc518b0dfe3de027a2
Grigori Fursin <grigori.fursin@cTuning.org>
2017-11-10 16:47:20 +0100

    fixed CLANG 5.0.0 installer for Ubuntu

--- package/compiler-llvm-5.0.0-universal/custom.py
+++ package/compiler-llvm-5.0.0-universal/custom.py
@@ -142,12 +142,12 @@ def setup(i):
           if 'debian' in flavor:
              nie['PACKAGE_NAME']='clang+llvm-5.0.0-aarch64-linux-gnu.tar.xz'
           else:
-             nie['PACKAGE_NAME']='clang+llvm-5.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz'
+             nie['PACKAGE_NAME']='clang+llvm-5.0.0-x86_64-linux-ubuntu-16.04.tar.xz'
 
              if mver=='14.04':
-                nie['PACKAGE_NAME']='clang+llvm-5.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz'
+                nie['PACKAGE_NAME']='clang+llvm-5.0.0-x86_64-linux-ubuntu-14.04.tar.xz'
              elif mver=='16.10':
-                nie['PACKAGE_NAME']='clang+llvm-5.0.0-x86_64-linux-gnu-ubuntu-16.10.tar.xz'
+                nie['PACKAGE_NAME']='clang+llvm-5.0.0-x86_64-linux-ubuntu-16.10.tar.xz'
 
        nie['PACKAGE_UNXTAR']='YES'
        nie['PACKAGE_UNTAR_EXTRA']='--strip 1'
