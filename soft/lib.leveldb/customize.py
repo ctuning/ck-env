9124a8f659906eb27e07c04b090eab34711b6b7e
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-03-05 14:45:40 +0000

    Introducing a new 'soft' entry for LevelDB library detection.
    Up to now LevelDB has been an invisible dependency of Caffe packages.
    Let's take it out of the closet.
    
    I tried to make the customize.py as generic as possible,
    to potentially reuse the code without modifications in future.

---
+++ soft/lib.leveldb/customize.py
@@ -0,0 +1,100 @@
+#
+# Collective Knowledge (individual environment - setup)
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+# Developer: Leo Gordon @ dividiti
+#
+
+import os
+
+##############################################################################
+# setup environment setup
+
+def setup(i):
+    """
+    Input:  {
+              cfg              - meta of this soft entry
+              self_cfg         - meta of module soft
+              ck_kernel        - import CK kernel module (to reuse functions)
+
+              host_os_uoa      - host OS UOA
+              host_os_uid      - host OS UID
+              host_os_dict     - host OS meta
+
+              target_os_uoa    - target OS UOA
+              target_os_uid    - target OS UID
+              target_os_dict   - target OS meta
+
+              target_device_id - target device ID (if via ADB)
+
+              tags             - list of tags used to search this entry
+
+              env              - updated environment vars from meta
+              customize        - updated customize vars from meta
+
+              deps             - resolved dependencies for this soft
+
+              interactive      - if 'yes', can ask questions, otherwise quiet
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+
+              bat          - prepared string for bat file
+            }
+
+    """
+
+    import os
+
+    # Get variables
+    ck      = i['ck_kernel']
+    cus     = i.get('customize',{})
+    fp      = cus.get('full_path','')
+
+    install_root = os.path.dirname( os.path.dirname( fp ) )
+    found=False
+    while True:
+       if os.path.isdir(os.path.join(install_root,'lib')):
+          found=True
+          break
+       up_one_level = os.path.dirname(install_root)
+       if up_one_level == install_root:
+          break
+       install_root = up_one_level 
+
+    if not found:
+       return {'return':1, 'error':'can\'t find root dir of this installation'}
+
+    env                     = i['env']
+    hosd                    = i['host_os_dict']
+    tosd                    = i['target_os_dict']
+    file_extensions         = hosd.get('file_extensions',{})    # not clear whether hosd or tosd should be used in soft detection
+    file_root_name          = cus['file_root_name']
+    static_lib_name         = file_root_name + file_extensions.get('lib','')
+    dynamic_lib_name        = file_root_name + file_extensions.get('dll','')
+    env_prefix              = cus['env_prefix']
+    path_lib                = os.path.join(install_root,'lib')
+
+    cus['path_lib']                 = path_lib
+    cus['path_include']             = os.path.join(install_root,'include')
+    cus['path_bin']                 = os.path.join(install_root,'bin')
+    cus['static_lib']               = static_lib_name
+    cus['dynamic_lib']              = dynamic_lib_name
+
+    env[env_prefix]                 = install_root
+    env[env_prefix+'_STATIC_NAME']  = static_lib_name
+    env[env_prefix+'_DYNAMIC_NAME'] = dynamic_lib_name
+
+    r = ck.access({'action': 'lib_path_export_script', 
+                   'module_uoa': 'os', 
+                   'host_os_dict': hosd, 
+                   'lib_path': path_lib })
+    if r['return']>0: return r
+    shell_setup_script_contents = r['script']
+
+    return {'return': 0, 'bat': shell_setup_script_contents}
