2a3c6a596eb5f00931c7205ff8d992177e987a00
Flavio Vella <flavio@dividiti.com>
2018-04-17 10:09:48 +0100

    Add lib-metis

---
+++ package/lib-metis-5.1.0/scripts.linux/install.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+#
+# Extra installation script
+#
+# See CK LICENSE.txt for licensing details.
+# See CK COPYRIGHT.txt for copyright details.
+#
+# Developer(s):
+# - Grigori Fursin, grigori.fursin@cTuning.org, 2017
+#
+
+cd ${INSTALL_DIR}/${PACKAGE_SUB_DIR}
+
+PREFIX=${INSTALL_DIR}/install
+
+make config shared=1 prefix=$PREFIX cc=${CK_CC}
+if [ "${?}" != "0" ] ; then
+    echo "Error: configure failed!"
+      exit 1
+fi
+
+
+
+make install;
+if [ "${?}" != "0" ] ; then
+    echo "Error: make installation failed!"
+      exit 1
+fi
+
+#make check
