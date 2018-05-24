7aa8cd55c1ef72da4ef33b1a3a81e5e1a03f2f81
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-05-24 17:05:21 +0200

    added universal handling of templates for new entries in misc module

--- module/misc/module.py
+++ module/misc/module.py
@@ -329,3 +329,184 @@ def add_key(i):
         if r['return']>0: return r
 
     return {'return':0}
+
+##############################################################################
+# internal file to ignore files/directories from templates
+
+def prepare_entry_template_ignore_files(dr, files):
+    lst=['.cm','tmp']
+    return lst
+
+##############################################################################
+# prepare template for a given entry
+# (trying to unify templates for CK when adding program, soft, package, program, dataset, etc)
+
+def prepare_entry_template(i):
+    """
+    Input:  {
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+
+    import os
+    import shutil
+
+    o=i.get('out','')
+    oo=''
+    if o=='con': oo=o
+
+    omuoa=i.get('original_module_uoa','')
+    ruoa=i.get('repo_uoa','')
+    duoa=i.get('data_uoa','')
+
+    # Search templates
+    tuoa=i.get('template','')
+    truoa=i.get('template_repo','')
+
+    if tuoa=='':
+       lst=[{'data_uid':'', 'data_uoa':'', 'info':{'data_name':'Empty entry'}, 'meta':{'sort':9999}}] # Add it to the end
+
+       if tuoa=='':
+          ii={'action':'search',
+              'module_uoa':omuoa,
+              'data_uoa':tuoa,
+              'search_dict':{'template':'yes'}}
+          r=ck.access(ii)
+          if r['return']>0: return r
+
+          lst+=r['lst']
+
+       # Make selection
+       if oo=='con':
+          r=select_uoa({'text':'Select template for the new entry', 
+                        'choices':lst})
+          if r['return']>0: return r
+
+          c=r['choice']
+
+          tuoa=c['data_uid']
+          truoa=c['repo_uid']
+
+    d=i.get('dict',{})
+
+    if tuoa!='':
+       # Attempt to load entry
+       ii={'action':'load',
+           'module_uoa':omuoa,
+           'data_uoa':tuoa,
+           'repo_uoa':truoa}
+       r=ck.access(ii)
+       if r['return']>0: return r
+
+       p=os.path.realpath(r['path'])
+
+       d.update(r['dict'])
+
+    i['action']='add'
+    i['module_uoa']=omuoa
+
+    i['common_func']='yes'
+    i['sort_keys']='yes'
+
+    i['dict']=d
+    
+    r=ck.access(i)
+    if r['return']>0: return r
+
+    # Copy files to a new entry if template
+    if tuoa!='':
+       pnew=r['path']
+
+       d=os.listdir(p)
+       for f in d:
+           if f!='.cm' and not f.endswith('.pyc') and 'tmp' not in f:
+              porig2=os.path.join(p,f)
+              pnew2=os.path.join(pnew,f)
+
+              try:
+
+                 if os.path.isdir(porig2):
+                    shutil.copytree(porig2, pnew2, ignore=shutil.ignore_patterns('.cm', '*tmp*', '*.pyc'))
+                 else:
+                    shutil.copyfile(porig2, pnew2)
+
+              except IOError as e: 
+                 return {'return':1, 'error':'problem copying files from template ('+str(e)+')'}
+
+    return r
+
+##############################################################################
+# Universal UOA selector (improved version forked 
+# from ck-autotuning:module:choice and ck.kernel)
+
+def select_uoa(i):
+    """
+    Input:  {
+              choices      - list from search function
+              (text)       - selection text
+              (skip_enter) - if 'yes', do not select 0 when user presses Enter
+              (skip_sort)  - if 'yes', do not sort array
+            }
+
+    Output: {
+              return  - return code =  0, if successful
+                                    >  0, if error
+              (error) - error text if return > 0
+              choice  - {dict of selection from lst}
+            }
+
+    """
+
+    se=i.get('skip_enter','')
+
+    lst=i.get('choices',[])
+
+    # Prepare data_name and then data_uoa
+
+    if i.get('skip_sort','')!='yes':
+       slst=sorted(lst, key=lambda v: (v.get('meta',{}).get('sort',0), v.get('info',{}).get('data_name',''), v['data_uoa']))
+    else:
+       slst=lst
+
+    array={}
+    n=0
+
+    for x in slst:
+        sn=str(n)
+        array[sn]=x
+
+        duoa=x['data_uoa']
+        name=x.get('info',{}).get('data_name','')
+        if name=='': name=duoa
+
+        s=sn+') '+name
+        if duoa!='': s+=' (--template='+duoa+')'
+
+        ck.out(s)
+
+        n+=1
+
+    ck.out('')
+
+    text=i.get('text','')
+    if text=='': text='Select UOA'
+
+    s=text
+    if se!='yes': s+=' (or press Enter for 0)'
+    s+=': '
+
+    rx=ck.inp({'text':s})
+    y=rx['string'].strip()
+
+    if y=='' and se!='yes': y='0' 
+
+    if y not in array:
+       return {'return':1, 'error':'number is not recognized'}
+
+    return {'return':0, 'choice':array[y]}
