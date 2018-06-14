28e29ef44944f40ba4f86a549e0797e86f67a20d
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-06-14 15:41:25 +0100

    a possible technique to improve version testing for python packages (needs more experimentation)

--- soft/lib.python.scipy/customize.py
+++ soft/lib.python.scipy/customize.py
@@ -6,6 +6,7 @@
 #
 
 import os
+import sys
 
 ##############################################################################
 
@@ -20,7 +21,10 @@ def version_cmd(i):
     version_module_name     = cus.get('version_module_name', '__init__')
     version_variable_name   = cus.get('version_variable_name', '__version__')
 
+    #sys.path.insert(0, site_dir)    # temporarily prepend site_dir to allow the potential recursive imports to work
     rx=ck.load_module_from_path({'path':path_without_init_py, 'module_code_name':version_module_name, 'skip_init':'yes'})
+    #sys.path.pop(0)                 # retain the original module search path
+
     if rx['return']==0:
         loaded_package  = rx['code']
         version_string  = getattr(loaded_package, version_variable_name)
