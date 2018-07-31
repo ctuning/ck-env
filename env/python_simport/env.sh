d9b505f2cf0e4ac611b349e307bb83d66a968a2d
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-07-31 14:59:25 +0100

    generate quasi-unique env.variable to detect that the script has already been loaded (Unix only!)

--- env/python_simport/env.sh
+++ env/python_simport/env.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ "$1" != "1" ]; then if [ "$CK_ENV_PYTHON_SIMPORT_SET" == "1" ]; then return; fi; fi
-
 # Finding the path to the directory that contains THIS bash script,
 # the solution borrowed from here:
 #       https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
 
 PATH_TO_THIS_ENTRY_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
+ALREADY_LOADED_NAME=CK_STATIC_ENV_`basename $PATH_TO_THIS_ENTRY_DIR | tr -cd '[:alnum:]_'`
+ALREADY_LOADED_FLAG=${ALREADY_LOADED_NAME}
+
+if [ "$1" != "1" ] && [ "$ALREADY_LOADED_FLAG" == "1" ]; then return; fi;
 
 echo Adding $PATH_TO_THIS_ENTRY_DIR to the PYTHONPATH ...
 
 export PYTHONPATH=${PATH_TO_THIS_ENTRY_DIR}:${PYTHONPATH}
 
-export CK_ENV_PYTHON_SIMPORT_SET=1
+export ${ALREADY_LOADED_NAME}=1
