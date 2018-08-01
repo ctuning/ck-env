991797ef179caa6519a976e79d57c8240ee1632d
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-08-01 16:18:53 +0100

    generate quasi-unique env.variable to detect that the script has already been loaded (Windows attempt1)

--- env/python_simport/env.bat
+++ env/python_simport/env.bat
@@ -1,18 +1,22 @@
 @echo off
 
-if not [%1] == [1] (if defined CK_ENV_PYTHON_SIMPORT_SET exit /b 0)
-
 rem Finding the path to the directory that contains THIS batch script,
 rem the solution borrowed from here:
 rem       https://stackoverflow.com/questions/8797983/can-a-windows-batch-file-determine-its-own-file-name
 rem       https://stackoverflow.com/questions/778135/how-do-i-get-the-equivalent-of-dirname-in-a-batch-file/778147
 
 set PATH_TO_THIS_ENTRY_DIR=%~dp0
+set TRIMMED_PATH=%PATH_TO_THIS_ENTRY_DIR:~0,-1%
+for /f %j in ("%TRIMMED_PATH%") do set ALREADY_LOADED_NAME=CK_STATIC_ENV_%~nxj
+
+echo %ALREADY_LOADED_NAME%
+
+if not [%1] == [1] (if defined %ALREADY_LOADED_NAME% exit /b 0)
 
 echo Adding %PATH_TO_THIS_ENTRY_DIR% to the PYTHONPATH ...
 
 set PYTHONPATH=%PATH_TO_THIS_ENTRY_DIR%;%PYTHONPATH%
 
-set CK_ENV_PYTHON_SIMPORT_SET=1
+set %ALREADY_LOADED_NAME%=1
 
 exit /b 0
