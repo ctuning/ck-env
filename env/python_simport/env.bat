22cd35e557f86471b7fe7c2ed27f96940897841c
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-08-01 16:40:28 +0100

    generate quasi-unique env.variable to detect that the script has already been loaded (Windows attempt2)

--- env/python_simport/env.bat
+++ env/python_simport/env.bat
@@ -6,10 +6,7 @@ rem       https://stackoverflow.com/questions/8797983/can-a-windows-batch-file-d
 rem       https://stackoverflow.com/questions/778135/how-do-i-get-the-equivalent-of-dirname-in-a-batch-file/778147
 
 set PATH_TO_THIS_ENTRY_DIR=%~dp0
-set TRIMMED_PATH=%PATH_TO_THIS_ENTRY_DIR:~0,-1%
-for /f %j in ("%TRIMMED_PATH%") do set ALREADY_LOADED_NAME=CK_STATIC_ENV_%~nxj
-
-echo %ALREADY_LOADED_NAME%
+for /f %%j in ("%PATH_TO_THIS_ENTRY_DIR:~0,-1%") do set ALREADY_LOADED_NAME=CK_STATIC_ENV_%%~nxj
 
 if not [%1] == [1] (if defined %ALREADY_LOADED_NAME% exit /b 0)
 
