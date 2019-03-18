05615a86af7503fc7cd93153c7f537205452ed42
Grigori Fursin <Grigori.Fursin@cTuning.org>
2019-03-18 13:10:58 +0000

    added openjdk 9,10,11

---
+++ package/openjdk-11.28/custom.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://fursin.net
+#
+
+import os
+import sys
+import json
+
+##############################################################################
+# customize installation
+
+def setup(i):
+    """
+    Input:  {
+              cfg              - meta of this soft entry
+              self_cfg         - meta of module soft
+              ck_kernel        - import CK kernel module (to reuse functions)
+
+              host_os_uoa      - host OS UOA
+              host_os_uid      - host OS UID
+              host_os_dict     - host OS meta
+
+              target_os_uoa    - target OS UOA
+              target_os_uid    - target OS UID
+              target_os_dict   - target OS meta
+
+              target_device_id - target device ID (if via ADB)
+
+              tags             - list of tags used to search this entry
+
+              env              - updated environment vars from meta
+              customize        - updated customize vars from meta
+
+              deps             - resolved dependencies for this soft
+
+              interactive      - if 'yes', can ask questions, otherwise quiet
+
+              path             - path to entry (with scripts)
+              install_path     - installation path
+            }
+
+    Output: {
+              return        - return code =  0, if successful
+                                          >  0, if error
+              (error)       - error text if return > 0
+
+              (install_env) - prepare environment to be used before the install script
+            }
+
+    """
+
+    import os
+    import shutil
+
+    # Get variables
+    o=i.get('out','')
+
+    ck=i['ck_kernel']
+
+    hos=i['host_os_uoa']
+    tos=i['target_os_uoa']
+
+    hosd=i['host_os_dict']
+    tosd=i['target_os_dict']
+
+    hbits=hosd.get('bits','')
+    tbits=tosd.get('bits','')
+
+    hname=hosd.get('ck_name','')    # win, linux
+    hname2=hosd.get('ck_name2','')  # win, mingw, linux, android
+    macos=hosd.get('macos','')      # yes/no
+
+    hft=i.get('features',{}) # host platform features
+    habi=hft.get('os',{}).get('abi','') # host ABI (only for ARM-based); if you want to get target ABI, use tosd ...
+                                        # armv7l, etc...
+
+    p=i['path']
+
+    env=i['env']
+
+    pi=i.get('install_path','')
+
+    cus=i['customize']
+    ie=cus.get('install_env',{})
+    nie={} # new env
+
+    # Update vars
+    f=ie['PACKAGE_NAME_TEMPLATE']
+
+    if hbits=='32':
+       return {'return':1, 'error':'this package doesn\'t support 32-bit OS'}
+
+    if hname=='win':
+       f+='windows-x64_bin.tar'
+
+       nie['PACKAGE_WGET_EXTRA']=ie['PACKAGE_WGET_EXTRA']+' -O '+f+'.gz'
+    else:
+       f+='linux-x64_bin.tar'
+
+    nie['PACKAGE_UNGZIP']='YES'
+    nie['PACKAGE_UNTAR']='YES'
+
+    nie['PACKAGE_NAME1']=f
+    f+='.gz'
+
+    nie['PACKAGE_NAME']=f
+
+    return {'return':0, 'install_env':nie}
