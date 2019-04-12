790a777baec95546565bd4582d25e7af302c8531
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-04-12 17:22:08 +0100

    allow to redefine _SUBSET_OFFSET, _SUBSET_VOLUME and provide _SUBSET_FOF

--- package/dataset-imagenet-preprocessed/preprocess_image_dataset.py
+++ package/dataset-imagenet-preprocessed/preprocess_image_dataset.py
@@ -67,27 +67,41 @@ def load_image(image_path,            # Full path to processing image
   return img
 
 
-def preprocess_directory(source_dir, destination_dir, crop_factor, square_side, inter_size, convert_to_bgr):
+def preprocess_directory(source_dir, destination_dir, crop_factor, square_side, inter_size, convert_to_bgr, offset, volume_str, fof_name):
     "Go through the given directory and preprocess all the files"
 
-    done_count = 0
-    
-    for filename in sorted(os.listdir(source_dir)):
-        for extension in supported_extensions:
-            if filename.lower().endswith( extension ):
-                full_input_path     = os.path.join(source_dir, filename)
-                full_output_path    = os.path.join(destination_dir, filename)
+    sorted_filenames = [filename for filename in sorted(os.listdir(source_dir)) if any(filename.lower().endswith(extension) for extension in supported_extensions) ]
 
-                image_data = load_image(image_path = full_input_path,
-                                      target_size = square_side,
-                                      intermediate_size = inter_size,
-                                      crop_percentage = crop_factor,
-                                      convert_to_bgr = convert_to_bgr)
-                image_data.tofile(full_output_path)
+    total_volume = len(sorted_filenames)
 
-                done_count += 1
-                print("{}:  Stored {}".format(done_count, full_output_path) )
+    if offset<0:        # support offsets "from the right"
+        offset += total_volume
 
+    volume = int(volume_str) if len(volume_str)>0 else total_volume-offset
+
+
+#    selected_filenames = sorted_filenames[offset:min(offset+volume,total_volume)]
+    selected_filenames = sorted_filenames[offset:offset+volume]
+
+    for current_idx in range(len(selected_filenames)):
+        filename = selected_filenames[current_idx]
+
+        full_input_path     = os.path.join(source_dir, filename)
+        full_output_path    = os.path.join(destination_dir, filename)
+
+        image_data = load_image(image_path = full_input_path,
+                              target_size = square_side,
+                              intermediate_size = inter_size,
+                              crop_percentage = crop_factor,
+                              convert_to_bgr = convert_to_bgr)
+        image_data.tofile(full_output_path)
+
+        print("[{}]:  Stored {}".format(current_idx+1, full_output_path) )
+
+    fof_full_path = os.path.join(destination_dir, fof_name)
+    with open(fof_full_path, 'w') as fof:
+        for filename in selected_filenames:
+            fof.write(filename + '\n')
 
 if __name__ == '__main__':
     import sys
@@ -99,8 +113,11 @@ if __name__ == '__main__':
     crop_factor     = float( os.environ['_CROP_FACTOR'] )
     inter_size      = int( os.getenv('_INTERMEDIATE_SIZE', 0) )
     convert_to_bgr  = os.getenv('_CONVERT_TO_BGR', '').lower() == 'yes'
+    offset          = int( os.getenv('_SUBSET_OFFSET', 0) )
+    volume_str      = os.getenv('_SUBSET_VOLUME', '' )
+    fof_name        = os.getenv('_SUBSET_FOF', 'fof.txt')
 
-    print("From: {} , To: {} , Size: {} , Crop: {} , InterSize: {} , 2BGR: {}".format(source_dir, destination_dir, square_side, crop_factor, inter_size, convert_to_bgr) )
+    print("From: {} , To: {} , Size: {} , Crop: {} , InterSize: {} , 2BGR: {}, OFF: {}, VOL: '{}', FOF: {}".format(
+        source_dir, destination_dir, square_side, crop_factor, inter_size, convert_to_bgr, offset, volume_str, fof_name) )
 
-    preprocess_directory(source_dir, destination_dir, crop_factor, square_side, inter_size, convert_to_bgr)
-
+    preprocess_directory(source_dir, destination_dir, crop_factor, square_side, inter_size, convert_to_bgr, offset, volume_str, fof_name)
