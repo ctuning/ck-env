ab5d137aa337efbfabc4ec03f4c74cbf928e0d54
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-05-29 17:15:38 +0100

    Expose binary path that comes with the python-package. NB: watch out for potential path contamination!

--- soft/lib.python.scipy/customize.py
+++ soft/lib.python.scipy/customize.py
@@ -106,9 +106,17 @@ def setup(i):
 
     full_path               = cus.get('full_path','')
     path_lib                = os.path.dirname(full_path)
-    path_install            = os.path.dirname(path_lib)
+    path_build              = os.path.dirname(path_lib)
 
     env                     = i['env']
-    env['PYTHONPATH']       = path_install + ( ';%PYTHONPATH%' if winh=='yes' else ':${PYTHONPATH}')
+    env['PYTHONPATH']       = path_build + ( ';%PYTHONPATH%' if winh=='yes' else ':${PYTHONPATH}')
+
+    # The following assumes the package has been installed (rather than detected) :
+    #
+    path_install            = os.path.dirname(path_build)
+    path_bin                = os.path.join(path_install, 'python_deps_site', 'bin')
+
+    if os.path.isdir(path_bin):
+        env['PATH']         = path_bin + ( ';%PATH%' if winh=='yes' else ':${PATH}')
 
     return {'return':0, 'bat':''}
