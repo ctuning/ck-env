8dc61298c8669339ba928b6dbc4350ebc5354b65
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-06-07 17:32:40 +0100

    support mutually-exclusive variations of standalone-toolchain (gnustl, libc++ and stlport)

--- package/tool-standalone-toolchain/install.sh
+++ package/tool-standalone-toolchain/install.sh
@@ -13,8 +13,14 @@
 #
 #   _TARGET_ARCH    ("arm64" by default)
 #   _API_LEVEL      ("24" by default -- corresponds to Android 7.0)
+#	_WHICH_STL		("" by default, but can also be "gnustl", "libc++" or "stlport")
 
-${CK_ANDROID_NDK_ROOT_DIR}/build/tools/make_standalone_toolchain.py \
+STL_OPT=""
+if [ -n "$_WHICH_STL" ]; then
+	STL_OPT="--stl ${_WHICH_STL}"
+fi
+
+${CK_ANDROID_NDK_ROOT_DIR}/build/tools/make_standalone_toolchain.py ${STL_OPT} \
     --arch ${_TARGET_ARCH} \
     --api ${_API_LEVEL} \
     --install-dir ${INSTALL_DIR}/install
