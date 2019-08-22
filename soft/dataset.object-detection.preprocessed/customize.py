c91004738accf5e4b13023fe73bbff692ffe6283
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-08-22 16:44:47 +0100

    automated detection of a pre-created (e.g. copied over) preprocessed dataset with an obligatory index file

--- soft/dataset.object-detection.preprocessed/customize.py
+++ soft/dataset.object-detection.preprocessed/customize.py
@@ -49,15 +49,40 @@ def setup(i):
     """
 
     import os
+    from math import sqrt
 
+    ck_kernel       = i.get('ck_kernel')
     cus             = i.get('customize',{})
     full_path       = cus.get('full_path','')
     env_prefix      = cus.get('env_prefix','')
     install_env     = cus.get('install_env', {})
+    detection_mode  = len(install_env) == 0
 
     path_install    = full_path if os.path.isdir(full_path) else os.path.dirname(full_path)
     env             = i.get('env', {})
     env[env_prefix + '_DIR'] = path_install
+
+    if detection_mode:
+        preprocessed_ext    = cus.get("detect_preprocessed_ext")
+        bytes_per_pixel     = cus.get("detect_bytes_per_pixel")
+        file_index_name     = cus.get("detect_file_index_name")
+        full_index_path     = os.path.join(path_install, file_index_name)
+
+        if os.path.exists(full_index_path):
+            r=ck_kernel.load_text_file({'text_file':full_index_path})
+            if r['return']>0: return
+            file_and_size_index = r['string'].split("\n")[:-1]
+            file_index          = [ name_and_size.split(';')[0] for name_and_size in file_and_size_index ]
+        else:
+            return {'return':1, 'error':'Detection failed since original dimensions are missing from index file {}'.format(full_index_path)}
+
+        install_env.update({
+            '_NEW_EXTENSION':       preprocessed_ext,
+            '_SUBSET_FOF':          file_index_name,
+            '_SUBSET_OFFSET':       0,
+            '_SUBSET_VOLUME':       len(file_index),
+            '_INPUT_SQUARE_SIDE':   int(sqrt( os.path.getsize(os.path.join(path_install,file_index[0]))/float(bytes_per_pixel) )),
+        })
 
     ## Prepend the hidden variables with env_prefix
     #
@@ -66,7 +91,7 @@ def setup(i):
             env[env_prefix + varname] = install_env[varname]
 
     dataset_source_name = 'dataset-source'
-    dataset_source_is_available = dataset_source_name in i['deps']
+    dataset_source_is_available = dataset_source_name in i.get('deps', {})
 
     def dep_env(dep, var): return i['deps'][dep]['dict']['env'].get(var)
 
