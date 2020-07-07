4ac0aa66a5525ed892b9099beabaf972777f0606
Leo Gordon <ens-lg4@users.noreply.github.com>
2020-07-07 15:12:17 +0100

    bugfix: another solution for lib/lib64 dilemma

--- package/lib-python-scipy/install.sh
+++ package/lib-python-scipy/install.sh
@@ -12,24 +12,24 @@
 # PYTHON_PACKAGE_NAME
 # PIP_INSTALL_OPTIONS
 
+echo "**************************************************************"
+echo ""
+echo "Cleanup: removing ${INSTALL_DIR}"
+rm -rf ${INSTALL_DIR:-nonexistent_dir}/*
 
     # This is where pip will install the modules.
     # It has its own funny structure we don't control :
     #
 EXTRA_PYTHON_SITE=${INSTALL_DIR}/python_deps_site
+mkdir -p ${EXTRA_PYTHON_SITE}/lib
+ln -s lib ${EXTRA_PYTHON_SITE}/lib64
 
 SHORT_PYTHON_VERSION=`${CK_ENV_COMPILER_PYTHON_FILE} -c 'import sys;print(sys.version[:3])'`
-export PACKAGE_LIB64_DIR="${EXTRA_PYTHON_SITE}/lib64/python${SHORT_PYTHON_VERSION}/site-packages"
 export PACKAGE_LIB_DIR="${EXTRA_PYTHON_SITE}/lib/python${SHORT_PYTHON_VERSION}/site-packages"
-export PYTHONPATH=$PACKAGE_LIB64_DIR:$PACKAGE_LIB_DIR:$PYTHONPATH
-
-echo "**************************************************************"
-echo ""
-echo "Cleanup: removing ${EXTRA_PYTHON_SITE}"
-rm -rf "${EXTRA_PYTHON_SITE}"
+export PYTHONPATH=$PACKAGE_LIB_DIR:$PYTHONPATH
 
 ######################################################################################
-echo "Installing '${PYTHON_PACKAGE_NAME}' and its dependencies to '$PACKAGE_LIB64_DIR' or '${PACKAGE_LIB_DIR}' ..."
+echo "Installing '${PYTHON_PACKAGE_NAME}' and its dependencies to '${PACKAGE_LIB_DIR}' ..."
 
 ${CK_ENV_COMPILER_PYTHON_FILE} -m pip install ${PYTHON_PACKAGE_NAME}${PACKAGE_VERSION:+"==${PACKAGE_VERSION}"} --prefix=${EXTRA_PYTHON_SITE} ${PIP_INSTALL_OPTIONS}
 
@@ -39,9 +39,7 @@ if [ "${?}" != "0" ] ; then
 fi
 
     # Because we have to provide a fixed name via meta.json ,
-    # and $PACKAGE_LIB64_DIR or $PACKAGE_LIB_DIR depends on the Python version,
+    # and $PACKAGE_LIB_DIR depends on the Python version,
     # we solve it by creating a symbolic link with a fixed name.
     #
-FOUND_PACKAGE_DIR=`find $EXTRA_PYTHON_SITE -name ${PYTHON_PACKAGE_DIR_NAME:-$PYTHON_PACKAGE_NAME} -type d`
-FOUND_PACKAGE_LIB_DIR=`dirname $FOUND_PACKAGE_DIR`
-ln -s $FOUND_PACKAGE_LIB_DIR ${INSTALL_DIR}/build
+ln -s $PACKAGE_LIB_DIR ${INSTALL_DIR}/build
